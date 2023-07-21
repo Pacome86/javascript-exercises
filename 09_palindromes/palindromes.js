9d058cb973c474ea5203913f52008320b72539af
Pacôme MARTIN <pacome.martin@hotmail.fr>
2023-07-21 16:28:17 +0200

    Pass all tests of palindromes.spec.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let punctuation = /[\.,?!]/g,
+        newString = string.toLowerCase()
+            .replace(punctuation, ''),
+        array,
+        text1,
+        text2;
+    
+    if (newString.includes(' ')) {
+        array = newString.split(' ');
+    } else {
+        array = newString.split('');
+    }
+
+    text1 = array.join('');
+    text2 = text1.split('').reverse().join('');
+
+    if (text1 === text2) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
