b3e2ab7dc58e4dc32d7f1b6c332d0e1705011787
Pacôme MARTIN <pacome.martin@hotmail.fr>
2023-07-22 17:00:24 +0200

    Pass all tests of fibonacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    
+    if (n < 0) return "OOPS";
 
+    const fib = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib[fib.length - 1] + fib[fib.length - 2];
 };
 
 // Do not edit below this line
