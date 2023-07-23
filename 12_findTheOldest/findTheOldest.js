a19064dca8f3a792b79b00661f2d884972f87a60
Pacôme MARTIN <pacome.martin@hotmail.fr>
2023-07-23 16:06:34 +0200

    Pass all tests of findTheOldest.spec.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const oldest = people.reduce((obj, person) => {
+        if ('yearOfDeath' in person) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = (new Date()).getFullYear() - person.yearOfBirth;
+        }
+        return (obj.age > person.age) ? obj : person;
+    }, {});
 
+    return oldest;
 };
 
 // Do not edit below this line
